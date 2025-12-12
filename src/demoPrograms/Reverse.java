bc5830b81b601b85e48cc6d1f1abbb5d206c7e22
Anush Office <anush@spicegems.com>
2025-12-12 13:35:54 +0530

    Reverse done

--- src/demoPrograms/Reverse.java
+++ src/demoPrograms/Reverse.java
@@ -1,10 +1,23 @@
 package demoPrograms;
 
 public class Reverse {
+int rev = 0, rem;
+
+ 
+
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Reverse obj = new Reverse();
+		System.out.println(obj.ReverseNo(123));
+ 
 	}
+	public int ReverseNo(int no) {
+		 while(no>0) {
+			 rem = no%10;
+			 rev = rev*10+rem;
+			 no = no/10;
+		 }
+		 return rev;
+	 }
 
 }
