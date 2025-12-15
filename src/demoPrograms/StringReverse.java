a573dacb33a614f9300b553dd1ed25df3fbbd582
Anush Office <anush@spicegems.com>
2025-12-15 19:01:40 +0530

    Reverse String

---
+++ src/demoPrograms/StringReverse.java
@@ -0,0 +1,16 @@
+
+package demoPrograms;
+
+public class StringReverse {
+
+	public static void main(String[] args) {
+	String name = "Anush Panday";
+	char[] reverse = new char[name.length()];
+	for(int i=0; i<name.length(); i++) {
+	reverse[i] = name.charAt(name.length()-i-1);
+	}
+	
+System.out.println(reverse);
+	}
+
+}
